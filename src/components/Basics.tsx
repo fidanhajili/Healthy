ffbccafebd9de6f3e0e1282212c8e5335cd53184
fidanhajili <fidanhajili7@gmail.com>
2024-08-31 18:32:37 +0400

    last vers

--- src/components/Basics.tsx
+++ src/components/Basics.tsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 
 
 
